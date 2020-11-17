9d7736098afd5ff4887ee76fbd66c5d9a57f6f94
Max Nikitin <maxxnikitin90@yandex.ru>
2020-11-17 23:15:39 +0300

    change regular of link

--- models/article.js
+++ models/article.js
@@ -26,7 +26,7 @@ const articleSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator(str) {
-        const regExp = /^((http|https):\/\/)(www\.)?([a-zA-Z0-9.-]+)\.([a-zA-Z]+)([a-zA-Z0-9$%?/.-]+)?(#)?$/;
+        const regExp = /^((http|https):\/\/)(www\.)?([A-Za-z0-9.-]{1,256})\.[A-Za-z]{2,10}([-a-zA-Z0-9@:%_+.~#?&/=]{1,256})?$/;
         return regExp.test(str);
       },
       message: 'Введите ссылку',
